7a33a1ad2db9246c0901245e20b1ec4a3339d400
Praveen Pugazh <praveenpugazh14@gmail.com>
2021-05-30 18:13:13 +0530

    with js functionalities

--- app.js
+++ app.js
@@ -47,4 +47,72 @@ const datas = [
     status: "INACTIVE",
     active: "red",
   },
+  {
+    id: 7,
+    name: "Parth Shah",
+    partnerId: "OMA00021",
+    mobile: 8869845321,
+    status: "INACTIVE",
+    active: "red",
+  },
 ];
+
+const sideSection = document.querySelector(".side-section-content");
+
+const contactCardDetails = datas.map((data) => {
+  //create elements
+  const contactCardParent = document.createElement("div");
+  const contactCard = document.createElement("div");
+  const arrowDiv = document.createElement("div");
+
+  //adding class names
+  contactCardParent.classList.add("single-contact-card");
+  contactCard.classList.add("card-content");
+  arrowDiv.classList.add("card-arrow");
+  arrowDiv.innerHTML = '<i class="fas fa-arrow-circle-right"></i>';
+  const title = document.createElement("h4");
+  title.innerText = data.name;
+  const para = document.createElement("p");
+  para.innerHTML = `<span> ${data.partnerId}</span> | <span>${data.mobile} </span>`;
+  const status = document.createElement("h5");
+  status.innerText = data.status;
+
+  //appending children to parent nodes
+  contactCard.appendChild(title);
+  contactCard.appendChild(para);
+  contactCard.appendChild(status);
+  contactCardParent.appendChild(contactCard);
+  contactCardParent.appendChild(arrowDiv);
+  sideSection.appendChild(contactCardParent);
+
+  contactCardParent.addEventListener("click", (e) => {
+    sideSection.childNodes.forEach((child) => {
+      if (child.nodeType === 1) {
+        if (child.classList.contains("active")) {
+          child.classList.remove("active");
+        }
+      }
+    });
+    const detailsName = document.querySelector(".name-card");
+    const detailsId = document.querySelector(".partner-id-card");
+    const detailsStatus = document.querySelector(".status-card");
+    const detailsMobile = document.querySelector(".mobile-card");
+    contactCardParent.classList.toggle("active");
+    console.log(contactCard);
+
+    detailsName.innerText = contactCard.children[0].textContent;
+    let idTrim = contactCard.children[1].textContent;
+    idTrim = idTrim.substr(0, idTrim.indexOf(" | "));
+    let mobileTrim = contactCard.children[1].textContent;
+
+    mobileTrim = mobileTrim.substr(
+      mobileTrim.indexOf(" | ") + 2,
+      mobileTrim.length - 1
+    );
+
+    detailsId.innerText = idTrim;
+    detailsMobile.innerText = mobileTrim;
+
+    detailsStatus.innerText = contactCard.children[2].textContent;
+  });
+});
